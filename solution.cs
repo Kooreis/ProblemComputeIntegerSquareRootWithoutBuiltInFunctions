0d38d6a61e925fad29f7b45951fdb17f80b20131
Kooreis <kooreis.official@gmail.com>
2024-09-16 17:55:52 -0500

    Initial function setup with a condition to return the input if it is 0 or 1.

--- solution.cs
+++ solution.cs
@@ -1,27 +1,5 @@
-```javascript
 function integerSquareRoot(n) {
     if (n === 0 || n === 1) {
         return n;
     }
-    let start = 1;
-    let end = n;
-    let ans;
-    while (start <= end) {
-        let mid = Math.floor((start + end) / 2);
-        if (mid * mid === n) {
-            return mid;
-        }
-        if (mid * mid < n) {
-            start = mid + 1;
-            ans = mid;
-        } else {
-            end = mid - 1;
-        }
-    }
-    return ans;
 }
-
-console.log(integerSquareRoot(10));
-console.log(integerSquareRoot(16));
-console.log(integerSquareRoot(25));
-```
